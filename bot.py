d55971390864e61f2586315e936d02eb3b130b8c
Invalidd <54702353+Invalidd@users.noreply.github.com>
2019-09-06 01:37:41 -0500

    Returns game rank with /stats

--- bot.py
+++ bot.py
@@ -22,5 +22,12 @@ async def on_ready():
 async def exampleCommand(ctx):
     await ctx.send('This is an example of a Command Funtion')
 
-
+@client.command(name="stats")
+async def on_message(ctx, username, game):
+    hiveUrl = 'http://api.hivemc.com/v1/player/{}/{}'.format(username,game)
+    req = Request(hiveUrl, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    decodeJson = json.loads(webpage)
+    finalString = decodeJson["title"]
+    await ctx.send(finalString)
 client.run(TOKEN)
