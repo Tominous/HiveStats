c97349a60021a2a5454fb84d49d832634c6c10eb
NitinSachdeo <nitinsachdeo@gmail.com>
2019-10-31 03:58:37 -0400

    Rework of how table updates are scheduled     - Now using a timestamp table to ensure tables update properly       despite any outages     - Table constants use a namedtuple based class for easier management

--- hivestats/database/leaderboard.py
+++ hivestats/database/leaderboard.py
@@ -1,5 +1,7 @@
 import schedule
 import time
+from datetime import datetime, timedelta
+from typing import NamedTuple
 
 from psycopg2.extensions import AsIs
 
@@ -9,11 +11,26 @@ from .sql import Postgres
 LEADERBOARD_LENGTH = 1000  # Number of players on the Hive leaderboard
 API_MAX_CALL_SIZE = 200  # Max leaderboard entries retrievable per api call
 
-UPDATE_FREQUENCY = 5  # in mins
+SQL_NOW = "now()"  # Constant for the timestamp function used in postgres
+UNIT_DICT = {  # Shortcode mapping for time units
+    "s": timedelta(seconds=1),
+    "m": timedelta(minutes=1),
+    "h": timedelta(hours=1),
+    "d": timedelta(days=1),
+    "w": timedelta(weeks=1),
+}
 
-BP_LEADERBOARD = {
-    "table": "bp_leaderboard",
-    "columns": (
+
+class Table(NamedTuple):
+    table: str
+    columns: tuple
+    types: tuple
+    update_freq: str = None
+
+
+BP_LEADERBOARD = Table(
+    table="bp_leaderboard",
+    columns=(
         "index",
         "human_index",
         "uuid",
@@ -24,7 +41,7 @@ BP_LEADERBOARD = {
         "played",
         "username",
     ),
-    "types": (
+    types=(
         "int",
         "int",
         "varchar(32)",
@@ -35,7 +52,14 @@ BP_LEADERBOARD = {
         "int",
         "varchar(200)",
     ),
-}
+    update_freq="5m",
+)
+
+LAST_UPDATED = Table(
+    table="last_updated",
+    columns=("name", "updated"),
+    types=("varchar(200)", "timestamp"),
+)
 
 
 def scheduled_update():
@@ -43,17 +67,73 @@ def scheduled_update():
     """
     database = Postgres()
 
-    database.create_table(**BP_LEADERBOARD)
-    update_leaderborad(database, BP_LEADERBOARD, "bp")
+    database.create_table(
+        LAST_UPDATED.table, LAST_UPDATED.columns, LAST_UPDATED.types, raise_error=False
+    )
+    database.add_constraint(
+        LAST_UPDATED.table, LAST_UPDATED.columns[0], "unique", raise_error=False
+    )
 
-    schedule.every(UPDATE_FREQUENCY).minutes.do(update_leaderborad, database, BP_LEADERBOARD, "bp")
+    database.create_table(
+        BP_LEADERBOARD.table,
+        BP_LEADERBOARD.columns,
+        BP_LEADERBOARD.types,
+        raise_error=False,
+    )
+    database.add_constraint(
+        BP_LEADERBOARD.table, BP_LEADERBOARD.columns[0], "unique", raise_error=False
+    )
+    check_outdated(database, BP_LEADERBOARD)
+
+    schedule.every().minute.do(check_outdated, database, BP_LEADERBOARD)
 
     while True:
         schedule.run_pending()
-        time.sleep(60)
+        time.sleep(1)
 
 
-def update_leaderborad(database, data_table, game):
+def check_outdated(database, data_table):
+    """Checks if a table is outdated based on it's update frequency and updates it
+
+    Args:
+        database (Postgres): interface to interact with the database
+        data_table (dict): defines the structure of the table to upload data to
+    """
+    now = datetime.utcnow()
+    period = data_table.update_freq
+    length, unit = int(period[:-1]), period[-1:]
+
+    database.cursor.execute(
+        """
+            select %(update_col)s from %(update_table)s
+                where %(name_col)s = %(target)s
+        """,
+        {
+            "update_table": AsIs(LAST_UPDATED.table),
+            "name_col": AsIs(LAST_UPDATED.columns[0]),
+            "update_col": AsIs(LAST_UPDATED.columns[1]),
+            "target": data_table.table,
+        },
+    )
+
+    updated = database.cursor.fetchone()
+
+    if updated:
+        updated = updated[0]
+
+        if unit == "M":
+            if (
+                ((now.year - updated.year) * 12) + (now.month - updated.month)
+            ) < length:
+                return False
+        else:
+            if (now - updated) < (UNIT_DICT[unit] * length):
+                return False
+
+    update_leaderborad(database, data_table)
+
+
+def update_leaderborad(database, data_table, game="bp"):
     """Retrieve full leaderboard for specified game and upload it to database table
 
     Args:
@@ -72,7 +152,16 @@ def update_leaderborad(database, data_table, game):
 
     data = tuple(tuple(row.values()) for row in data)
 
-    database.replace_table(**data_table, values=data)
+    database.insert(
+        data_table.table, data_table.columns, data, conflict_key=data_table.columns[0]
+    )
+
+    database.insert(
+        LAST_UPDATED.table,
+        LAST_UPDATED.columns,
+        ((data_table.table, SQL_NOW),),
+        conflict_key=LAST_UPDATED.columns[0],
+    )
 
 
 def query_leaderboard(database: Postgres, game, start, length=1):
@@ -98,7 +187,7 @@ def query_leaderboard(database: Postgres, game, start, length=1):
                 where index >= %(start)s
                 limit %(limit)s;
         """,
-        {"game": AsIs(game), "start": AsIs(start), "limit": AsIs(length)}
+        {"game": AsIs(game), "start": AsIs(start), "limit": AsIs(length)},
     )
 
     return database.cursor.fetchall()
